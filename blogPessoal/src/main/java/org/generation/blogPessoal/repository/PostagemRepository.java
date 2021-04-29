3e411adc4bc1d31c198f63571f2c9a0ce1c1b010
unknown <maxdenyssonn@gmail.com>
2021-04-28 21:02:09 -0300

    BlogPessoal

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+import org.generation.blogPessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// aplicação responsável pela comunicação com o banco de dados 
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+	public List<Postagem> findAllByTituloContaingIgnoreCase(String titulo); // para instanciar tudo que estiver em titulo na classe model 
+	
+}
